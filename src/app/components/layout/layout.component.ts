5c0c7af784e37a4cd637d74d206925cfa94fef6a
h.simchi <simchi.hossein@mapnaec.com>
2025-06-29 14:56:42 +0330

    add: Sub heading

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { SubHeaderComponent } from "../sub-header/sub-header.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SubHeaderComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
---
+++ src/app/components/sub-header/sub-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'coffee-sub-header',
+  imports: [],
+  templateUrl: './sub-header.component.html',
+  styleUrl: './sub-header.component.css'
+})
+export class SubHeaderComponent {
+
+}
